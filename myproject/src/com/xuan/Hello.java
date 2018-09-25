8ac43b615b6e1a01686d3d4c0dde4ac47168f94a
user <user@es302n25>
2018-09-25 11:58:49 +0800

    initial

---
+++ myproject/src/com/xuan/Hello.java
@@ -0,0 +1,14 @@
+package com.xuan;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		int age = -1;
+		age = 19;
+		short num = 8;
+		float weight = (float)52.5;
+		System.out.println(weight);
+	}
+	
+}
